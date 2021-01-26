a802c4b5ef6c6a66935bb66f6442a70a5e4b9de5
opavelkachalo <opavelkachalo@gmail.com>
2021-01-26 19:00:10 +0300

    add materialUi components and handler functions to CreateRoomPage.js

--- api/views.py
+++ api/views.py
@@ -14,22 +14,33 @@ class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
     def post(self, request):
+        # if there wasn't any session on device, create new session with it's key
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
         serializer = self.serializer_class(data=request.data)
+
+        # if serializer's validation went successfully, get data from it
         if serializer.is_valid():
             guest_can_pause = serializer.data.get('guest_can_pause')
             votes_to_skip = serializer.data.get('votes_to_skip')
+
+            # each host is identified by it's session key
             host = self.request.session.session_key
             queryset = Room.objects.filter(host=host)
+
+            # existing of queryset means that user already has created a room once
+            # and to create new room, we need to change 'guest_can_pause' and 'votes_to_skip' fields
             if queryset.exists():
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+
+            # otherwise we need to create a room from zero, and put a session key as a host value
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
-            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)

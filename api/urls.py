f55182062886233c28cd9e0dcac0ad5bc908807e
opavelkachalo <opavelkachalo@gmail.com>
2021-01-27 19:20:12 +0300

    create Room.js, add GetRoomView to frontend app

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView
+from .views import RoomView, CreateRoomView, GetRoomView
 urlpatterns = [
     path('', RoomView.as_view(), name='home'),
-    path('create-room/', CreateRoomView.as_view(), name='create-view'),
+    path('create-room/', CreateRoomView.as_view(), name='create-room'),
+    path('get-room/', GetRoomView.as_view(), name='get-room'),
 ]

f0739ed776ebf1cdd13c0be08a583338278baee8
opavelkachalo <opavelkachalo@gmail.com>
2021-01-28 21:46:43 +0300

    add LeaveRoom view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoomView, JoinRoomView, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoomView, JoinRoomView, UserInRoom, LeaveRoom
 urlpatterns = [
-    path('', RoomView.as_view(), name='home'),
-    path('create-room/', CreateRoomView.as_view(), name='create-room'),
-    path('get-room/', GetRoomView.as_view(), name='get-room'),
-    path('join-room/', JoinRoomView.as_view(), name='join-room'),
-    path('user-in-room/', UserInRoom.as_view(), name='user-in-room'),
+    path('', RoomView.as_view()),
+    path('create-room/', CreateRoomView.as_view()),
+    path('get-room/', GetRoomView.as_view()),
+    path('join-room/', JoinRoomView.as_view()),
+    path('user-in-room/', UserInRoom.as_view()),
+    path('leave-room/', LeaveRoom.as_view()),
 ]
